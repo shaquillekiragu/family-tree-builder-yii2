cf67745f247be1e328ff75227b6338ffa5eebcef
Shaquille Kiragu <shaquille@toru.digital>
2025-06-26 16:02:21 +0100

    Restructure repo no match domino

---
+++ rest/assets/AppAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@admin';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset'
+    ];
+}
