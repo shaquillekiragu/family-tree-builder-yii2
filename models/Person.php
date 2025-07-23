0d09dc94721af51d55389b867fbbadbec7cf81be
Shaquille Kiragu <shaquille@toru.digital>
2025-07-23 10:19:06 +0100

    Tree and person models

--- models/Person.php
+++ models/Person.php
@@ -4,4 +4,12 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
-class Person extends ActiveRecord {}
+class Person extends ActiveRecord
+{
+    public $person_id_list;
+
+    public static function tableName()
+    {
+        return 'person';
+    }
+}
--- models/Tree.php
+++ models/Tree.php
@@ -3,5 +3,26 @@
 namespace app\models;
 
 use yii\db\ActiveRecord;
+use yii\helpers\Inflector;
+use app\models\Person;
+use app\models\Relationship;
 
-class Tree extends ActiveRecord {}
+class Tree extends ActiveRecord
+{
+    public $tree_id_list;
+
+    public static function tableName()
+    {
+        return 'tree';
+    }
+
+    public function getSlug()
+    {
+        return Inflector::slug($this->tree_name);
+    }
+
+    public function getPersons()
+    {
+        return $this->hasMany(Person::class, ['id' => 'person_id']);
+    }
+}
