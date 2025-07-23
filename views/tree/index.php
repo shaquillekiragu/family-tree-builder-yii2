2aae613491dd91753695fbeeb2aaa7e17ac89493
Shaquille Kiragu <shaquille@toru.digital>
2025-07-23 10:19:16 +0100

    Trees index view

--- views/tree/index.php
+++ views/tree/index.php
@@ -1,1 +1,18 @@
-<h1>Trees</h1>
+<section class="my-5 flex flex-col gap-3">
+    <h1>Trees</h1>
+    <ul>
+        <?php
+        $i = 0;
+        $i++;
+        foreach ($trees as $tree) { ?>
+            <li>
+                <h3>Tree <?= $i ?>:</h3>
+                <ul>
+                    <li>Tree Name: <?= $tree->tree_name ?></li>
+                    <li>Tree ID: <?= $tree->id ?></li>
+                    <li>User ID: <?= $tree->user_id ?></li>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
+</section>
