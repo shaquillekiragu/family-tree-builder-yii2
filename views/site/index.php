bd6165c0513fda86a7e66cf0cdb7d8aa1ec502d1
Shaquille Kiragu <shaquille@toru.digital>
2025-10-18 18:44:12 +0100

    build basic landing page

--- views/site/index.php
+++ views/site/index.php
@@ -1,11 +1,17 @@
 <?php
-
 $this->title = 'My Yii Application';
 ?>
 
-<main class="site-index w-screen h-full min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-800 to-purple-500">
-	<div class="text-white text-center">
-		<h1 class="text-4xl font-bold mb-4">Welcome to Family Tree Builder</h1>
-		<p class="text-xl">Build and explore your family tree</p>
+<main class="site-index w-full h-full min-h-screen flex flex-col items-center gap-28 bg-gradient-to-br from-0% from-purple-900 via-60% via-purple-800 to-100% to-purple-600 p-40">
+	<div class="w-full flex flex-col items-center gap-4 text-center text-white">
+		<h1 class="text-4xl font-bold">Welcome to the Family Tree Builder</h1>
+		<h2 class="text-2xl">Build and explore <span class="underline">your family tree</span></h2>
+	</div>
+
+	<div class="w-full flex flex-col items-center gap-3 *:text-lg">
+		<h3 class="text-white mb-3">Login or register to get started...</h3>
+		<a href="#" class="bg-white rounded-full w-40 py-3 text-center">Login</a>
+		<p class="text-white">or</p>
+		<a href="#" class="bg-white rounded-full w-40 py-3 text-center">Sign up</a>
 	</div>
 </main>
