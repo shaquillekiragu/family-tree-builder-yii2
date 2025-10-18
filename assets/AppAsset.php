00d49aa27ab314b6cfc13bfeae8ab63f0c0c98bb
Shaquille Kiragu <shaquille@toru.digital>
2025-10-18 17:34:51 +0100

    tailwind config

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,14 +18,14 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
-    ];
-    public $js = [];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
-    ];
+	public $basePath = '@webroot';
+	public $baseUrl = '@web';
+	public $css = [
+		'css/site.css',
+	];
+	public $js = [];
+	public $depends = [
+		'yii\web\YiiAsset',
+		// 'yii\bootstrap5\BootstrapAsset' // Commented out to avoid conflicts with Tailwind
+	];
 }
